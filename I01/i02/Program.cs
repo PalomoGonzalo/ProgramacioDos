96509018e4e36d32c60c90492909448bb4f58654
Palamo Gonzalo <palomogonzalomatias@gmail.com>
2022-03-22 20:31:21 -0300

    test de visual

---
+++ I01/i02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace i02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("teasteando atajos        ");
+
+            for (int i = 0; i < 2000; i++)
+            {
+                nombre = "asd";
+            }
+        }
+    }
+}
