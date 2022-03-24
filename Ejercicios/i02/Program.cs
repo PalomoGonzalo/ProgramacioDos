4e33f0464135151dafeb5dc4af3c7b9dfe35c57a
Palamo Gonzalo <palomogonzalomatias@gmail.com>
2022-03-23 23:41:29 -0300

    ejercicio de primo terminado

---
+++ Ejercicios/i02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace i02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero;
+
+            do
+            {
+                Console.WriteLine("Ingrese un numero mayor que cero ");
+            } while (!float.TryParse(Console.ReadLine(), out numero)||numero<0);
+
+            Console.WriteLine("Numero {0}, su cubo es {1} y su cuadrado es {2}",numero,numero*numero,numero*numero*numero);
+
+
+        }
+    }
+}
---
+++ Ejercicios/i03 numeros primos/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace i03_numeros_primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            int numeroPrimo;
+            string mensaje;
+             
+            do
+            {
+                Console.WriteLine("ingrese un numero para calcular hasta el numero primo o escriba salir para terminar ");
+                mensaje = Console.ReadLine();
+                if (int.TryParse(mensaje, out numeroPrimo))
+                {
+                    Console.WriteLine("entro");
+                    for (int i = 1; i<=numeroPrimo; i++)
+                    {
+                        if (Esprimo(i))
+                        {
+                            Console.WriteLine("Los numeros primos son {0}", i);
+                        }
+                    }
+                }
+            } while (mensaje!="salir");
+
+            Console.WriteLine("salio");
+
+
+        }
+
+        public static Boolean Esprimo(int n)
+        {
+            if (n != 1 && n != 0)
+            {
+                for (int i = 2; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        if (n == i)
+                        { 
+                            return true;
+                        }
+                        return false;
+                    }
+                }  
+            }
+            return false;
+                
+            
+        }
+    }
+}
